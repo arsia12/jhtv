59a223052d0251612310c5509b8d97df0805ec16
ilwon2607 <siw@mixd.co.kr>
2021-06-08 13:25:44 +0900

    210608 Exception 함수 return type 변경 / Comment 기본적인 CRUD 완료

--- back/src/app_modules/auth/auth.controller.ts
+++ back/src/app_modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common';
+import { RouterTag } from 'src/common/decorators/router_swagger_tag.decorator';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dto/user_login.dto';
 
-@Controller('auth')
+@RouterTag('auth')
 export class AuthController {
     constructor(
         private readonly authSevice : AuthService
--- back/src/app_modules/comment/comment.controller.ts
+++ back/src/app_modules/comment/comment.controller.ts
@@ -1,10 +1,12 @@
-import { Get, Post, Param, Body, Query } from '@nestjs/common';
+import { Get, Post, Param, Body, Query, Delete, Put } from '@nestjs/common';
 import { AbstractController } from 'src/common/abstract_controller';
+import { SwaggerPagination } from 'src/common/decorators/pagination.decorator';
 import { SwaggerParameter } from 'src/common/decorators/parameter.decotrator';
 import { RouterTag } from 'src/common/decorators/router_swagger_tag.decorator';
 import { SwaggerDecorators } from 'src/common/decorators/swagger.decorator';
 import { CommentService } from './comment.service';
 import { CreateCommentlDTO } from './dto/create_comment.dto';
+import { UpdateCommentDTO } from './dto/update_comment.dto';
 
 @RouterTag('comment')
 export class CommentController extends AbstractController {
@@ -12,8 +14,9 @@ export class CommentController extends AbstractController {
     super();
   }
 
-  @SwaggerDecorators('미완성')
+  @SwaggerDecorators('게시글 댓글 리스트')
   @SwaggerParameter('Board PK')
+  @SwaggerPagination(1, 100)
   @Get('board/:id')
   async getCommentByBoard(@Param('id') id: number, @Query() query) {
     const data = await this.commentService.getCommentByBoard(
@@ -24,11 +27,6 @@ export class CommentController extends AbstractController {
     return this.makeResponse({ data });
   }
 
-  @SwaggerDecorators('미완성')
-  @SwaggerParameter('User PK')
-  @Get('user/:id')
-  async getCommentByUser() {}
-
   @SwaggerDecorators('댓글 작성')
   @SwaggerParameter('Board PK')
   @Post(':id')
@@ -37,6 +35,22 @@ export class CommentController extends AbstractController {
     @Body() body: CreateCommentlDTO,
   ) {
     const data = await this.commentService.createComment(id, body);
+    return this.makeResponse({ data });
+  }
+
+  @SwaggerDecorators('댓글 수정')
+  @SwaggerParameter('Comment PK')
+  @Put(':id')
+  async updateComment(@Param('id') id: number, @Body() body: UpdateCommentDTO) {
+    const data = await this.commentService.updateComment(id, body);
+    return this.makeResponse({ data });
+  }
+
+  @SwaggerDecorators('댓글 삭제')
+  @SwaggerParameter('Comment PK')
+  @Delete(':id')
+  async deleteComment(@Param('id') id: number) {
+    const data = await this.commentService.deleteComment(id);
     return this.makeResponse({ data });
   }
 }
