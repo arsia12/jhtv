852c9036d9b64144aae4aae67115e95af703816a
ilwon126 <arsia12@naver.com>
2021-06-09 21:32:42 +0900

    210609 로그인처리

--- back/src/app_modules/auth/auth.controller.ts
+++ back/src/app_modules/auth/auth.controller.ts
@@ -1,20 +1,21 @@
 import { Body, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RouterTag } from 'src/common/decorators/router_swagger_tag.decorator';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dto/user_login.dto';
+import { AbstractController } from 'src/common/abstract_controller';
+import { SwaggerDecorators } from 'src/common/decorators/swagger.decorator';
 
-@ApiTags('auth')
 @RouterTag('auth')
-export class AuthController {
-    constructor(
-        private readonly authSevice : AuthService
-    ){}
+export class AuthController extends AbstractController {
+  constructor(private readonly authSevice: AuthService) {
+    super();
+  }
 
-    @ApiOperation({ summary : '로그인' })
-    @Post('login')
-    async login(@Body() body : UserLoginDto){
-        const user = await this.authSevice.validateUser(body);
-        return this.authSevice.login(user);
-    }
+  // Todo : 페이로드 유저정보도 넣어주세요
+  @SwaggerDecorators('로그인')
+  @Post('login')
+  async login(@Body() body: UserLoginDto) {
+    const data = await this.authSevice.validateUser(body);
+    return this.makeResponse({ data })
+  }
 }
